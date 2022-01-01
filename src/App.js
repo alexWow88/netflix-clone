eb3b8af3120747d7fbbe8a763298744c4b9a618f
Alex Wower <woweralexander@gmail.com>
2022-01-01 23:30:52 +0000

    set up requests const

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+// API key: e88c15bf17a3128cbd63551c683340ca
+// API read access token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODhjMTViZjE3YTMxMjhjYmQ2MzU1MWM2ODMzNDBjYSIsInN1YiI6IjYxZDBkYjI4NjA5NzUwMDA0NWNkYWYwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wi6oNgc1xOubYzFvLLXe7l8i3wvdAw_FZ-SCh1_xYgQ
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey Hey</h1>
     </div>
   );
 }
