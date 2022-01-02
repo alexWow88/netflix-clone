95adbf35f97ca437ed56873dfa7c095a87d032ce
Alex Wower <woweralexander@gmail.com>
2022-01-02 00:39:47 +0000

    pull movie posters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Row from './components/Row/Row';
+import requests from './requests';
 
 // API key: e88c15bf17a3128cbd63551c683340ca
 // API read access token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODhjMTViZjE3YTMxMjhjYmQ2MzU1MWM2ODMzNDBjYSIsInN1YiI6IjYxZDBkYjI4NjA5NzUwMDA0NWNkYWYwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wi6oNgc1xOubYzFvLLXe7l8i3wvdAw_FZ-SCh1_xYgQ
@@ -6,7 +8,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hey Hey</h1>
+      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
