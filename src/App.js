fdfe9edc84d0b793d472f4328e1121f07d26940d
Alex Wower <woweralexander@gmail.com>
2022-01-03 23:00:16 +0000

    all rows fetching for all movie categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Row from './components/Row/Row';
-import requests from './requests';
+import "./App.css";
+import Row from "./components/Row/Row";
+import requests from "./requests";
 
 // API key: e88c15bf17a3128cbd63551c683340ca
 // API read access token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODhjMTViZjE3YTMxMjhjYmQ2MzU1MWM2ODMzNDBjYSIsInN1YiI6IjYxZDBkYjI4NjA5NzUwMDA0NWNkYWYwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wi6oNgc1xOubYzFvLLXe7l8i3wvdAw_FZ-SCh1_xYgQ
@@ -8,8 +8,17 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
